b55338bb9f14e03166813140ddcf63090f8c6022
Ankur Seth <iankurseth@gmail.com>
2022-11-23 23:05:50 +0530

    Cleanup, tw and ny

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App () {
+	return (
+		<div className="App">
+			<header className="App-header bg-green-500 text-white flex min-h-screen">
+				<h2 className="m-auto text-4xl">pointed</h2>
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    function ({ addVariant }) {
+        addVariant('ch', '& > *');
+        addVariant('ch2', '& > * > *');
+        addVariant('ch3', '& > * > * > *');
+    }
+  ],
+}
