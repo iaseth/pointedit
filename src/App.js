3d04ccef164759db6202f927a24f1e0e2c0c6f52
Ankur Seth <iankurseth@gmail.com>
2022-11-27 10:06:59 +0530

    Added new entry component: PointApp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,1 @@
-
-
-
-function App () {
-	return (
-		<div className="App">
-			<header className="App-header bg-green-500 text-white flex min-h-screen">
-				<h2 className="m-auto text-4xl">pointed</h2>
-			</header>
-		</div>
-	);
-}
-
-export default App;
+export {default} from './PointApp/PointApp';
---
+++ src/PointApp/PointApp.js
@@ -0,0 +1,13 @@
+
+
+
+export default function PointApp () {
+
+	return (
+		<div className="App">
+			<header className="App-header bg-green-500 text-white flex min-h-screen">
+				<h2 className="m-auto text-4xl">PointApp</h2>
+			</header>
+		</div>
+	);
+}
