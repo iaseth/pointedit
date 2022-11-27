ffa855763a86e5294eabdf68f7369f07b8c4273e
Ankur Seth <iankurseth@gmail.com>
2022-11-27 13:26:33 +0530

    Removed sidebars and added tabs to Header

--- src/PointApp/Header/Header.js
+++ src/PointApp/Header/Header.js
@@ -1,11 +1,24 @@
 
 
 
-export default function Header ({TABS}) {
+export default function Header ({TABS, currentTabIndex, setCurrentTabIndex}) {
+	const tabItems = TABS.map((t, k) => {
+		   const current = k === currentTabIndex;
+		   let className = "pt-1";
+		   className += current ? " bg-slate-100" : " bg-green-200 hover:bg-green-300 cursor-pointer";
+
+		   return (
+				   <div key={k} className={className} onClick={() => current ? null : setCurrentTabIndex(k)}>
+						   <h4 className="px-4 py-4">{t.title}</h4>
+				   </div>
+		   );
+	});
 
 	return (
-		<header className="bg-green-200 px-4 py-4">
-			<h2>Header</h2>
+		<header className="bg-green-200 select-none">
+			<div className="flex ch:grow ch:basis-0 text-center max-w-3xl mx-auto">
+				{tabItems}
+			</div>
 		</header>
 	);
 }
--- src/PointApp/PointApp.js
+++ src/PointApp/PointApp.js
@@ -2,20 +2,14 @@ import React from 'react';
 
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
-import LeftSidebar from './LeftSidebar/LeftSidebar';
-import RightSidebar from './RightSidebar/RightSidebar';
 
-import Account from './Content/Account';
 import Home from './Content/Home';
 import Market from './Content/Market';
-import Recent from './Content/Recent';
 import Settings from './Content/Settings';
 
 const TABS = [
 	{title: "Home", char: "H", component: Home},
-	{title: "Recent", char: "R", component: Recent},
 	{title: "Market", char: "M", component: Market},
-	{title: "Account", char: "A", component: Account},
 	{title: "Settings", char: "S", component: Settings},
 ];
 
@@ -24,16 +18,14 @@ export default function PointApp () {
 	const CurrentComponent = TABS[currentTabIndex].component;
 
 	return (
-		<div className="App flex bg-slate-100">
-			<LeftSidebar {...{TABS, currentTabIndex, setCurrentTabIndex}} />
+		<div className="bg-slate-100">
 
-			<main className="grow bg-white">
-				<Header />
+			<main className="">
+				<Header {...{TABS, currentTabIndex, setCurrentTabIndex}} />
 				<CurrentComponent />
 				<Footer />
 			</main>
 
-			<RightSidebar />
 		</div>
 	);
 }
