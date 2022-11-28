b4ce4d1582c5fcef303ca62e0627c33d49c38d50
Ankur Seth <iankurseth@gmail.com>
2022-11-28 20:28:22 +0530

    Moved Aspect and Point components into separate files

---
+++ src/PointApp/Content/Home/EditNote/Point.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {EditableText} from './Utils';
+
+
+
+export default function Point ({k, point, updatePoint, addNewPoint}) {
+	const setText = (text) => {
+		point.text = text;
+		updatePoint(point);
+	};
+
+	const onEnter = () => {
+		addNewPoint(k+1);
+	};
+
+	return (
+		<div className="flex items-center">
+			<h4 className="grow">
+				<EditableText number={k+1} text={point.text} {...{setText, onEnter}} />
+			</h4>
+		</div>
+	);
+}
