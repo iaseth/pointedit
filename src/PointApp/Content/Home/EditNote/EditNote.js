5c799267df051786aa3e8fb3cd09d353ca89bd85
Ankur Seth <iankurseth@gmail.com>
2022-11-29 19:13:24 +0530

    Added more props to getDefaultNoteObject()

--- src/PointApp/Content/Home/EditNote/EditNote.js
+++ src/PointApp/Content/Home/EditNote/EditNote.js
@@ -21,16 +21,22 @@ const getDefaultNoteObject = (noteId, categoryId) => {
 		id: noteId,
 		createdAt: Date.now(),
 		modifiedAt: Date.now(),
+		modifiedCount: 0,
 		openedAt: Date.now(),
+		openedCount: 0,
 
 		// deleted notes show up in Trash
 		deleted: false,
+		deletedAt: 0,
 		// hidden notes can be shown with a toggle
 		hidden: false,
+		hiddenAt: 0,
 		// pinned notes are pinned at the top
 		pinned: false,
+		pinnedAt: 0,
 		// completed notes cannot have empty fields
 		completed: false,
+		completedAt: 0,
 
 		title: "",
 		description: "",
