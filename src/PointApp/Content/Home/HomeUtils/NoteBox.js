57e41c23ad2f17335ab6f19e59a8d672c0beaa41
Ankur Seth <iankurseth@gmail.com>
2022-12-01 19:45:49 +0530

    Added focus support to Theme

--- src/PointApp/Content/Home/HomeUtils/NoteBox.js
+++ src/PointApp/Content/Home/HomeUtils/NoteBox.js
@@ -13,31 +13,33 @@ function NDots ({n, className=""}) {
 
 export function NoteBox ({note, onClick}) {
 	return (
-		<article className="bg-white flex flex-col rounded shadow overflow-hidden cursor-pointer hover:ring hover:ring-green-500" onClick={onClick}>
+		<div className={note.categoryId}>
+			<article className="bg-white flex flex-col rounded shadow overflow-hidden cursor-pointer hover:ring hover:ring-500" onClick={onClick}>
 
-			<header className="grow flex ch:basis-0 ch:grow ch:px-2 ch:pt-8 ch:pb-2 text-center">
+				<header className="grow flex ch:basis-0 ch:grow ch:px-2 ch:pt-8 ch:pb-2 text-center">
 
-				<section className="bg-green-100">
-					<h5>
-						<span className="bg-green-500 text-white px-2 py-1 rounded"># aspects</span>
-					</h5>
-					<NDots n={note.aspectsCount} className="bg-green-600" />
-				</section>
+					<section className="bg-100">
+						<h5>
+							<span className="bg-500 text-white px-2 py-1 rounded"># aspects</span>
+						</h5>
+						<NDots n={note.aspectsCount} className="bg-600" />
+					</section>
 
-				<section className="bg-green-400">
-					<h5>
-						<span className="bg-white text-green-800 px-2 py-1 rounded"># points</span>
-					</h5>
-					<NDots n={note.pointsCount} className="rounded-full" />
-				</section>
+					<section className="bg-400">
+						<h5>
+							<span className="bg-white text-800 px-2 py-1 rounded"># points</span>
+						</h5>
+						<NDots n={note.pointsCount} className="rounded-full" />
+					</section>
 
-			</header>
+				</header>
 
-			<main className="px-3 py-3 border-t-2 border-green-500 text-center">
-				<h4 className="text-green-800">{note.title || "No title"}</h4>
-				<h6 className="text-slate-500">Note</h6>
-			</main>
+				<main className="px-3 py-3 border-t-2 border-500 text-center">
+					<h4 className="text-800">{note.title || "No title"}</h4>
+					<h6 className="text-slate-500">Note</h6>
+				</main>
 
-		</article>
+			</article>
+		</div>
 	);
 }
