8746cc521d4e313d7cff9713ec69f61922a76fb3
Ankur Seth <iankurseth@gmail.com>
2022-12-01 18:00:42 +0530

    Added service-worker for offline functionality

--- src/PointApp/Content/Home/ViewNote/AspectView.js
+++ src/PointApp/Content/Home/ViewNote/AspectView.js
@@ -19,20 +19,20 @@ export default function AspectView ({aspectId, dbFuncs}) {
 	}
 
 	return (
-		<section className="bg-white shadow px-4 py-2 border-l-4 border-green-500 ch:py-2">
-			<header>
+		<section className="bg-white shadow px-4 border-l-4 border-green-500 rounded-r">
+			<header className="py-2">
 				<h3 className="py-2">{aspect.title || <span className="faded">No title</span>}</h3>
-				<h4>{aspect.description || <span className="faded">No description</span>}</h4>
+				<p>{aspect.description || <span className="faded">No description</span>}</p>
 			</header>
 
 			<main>
-				<section className="px-4">
+				<section className="px-4 py-2">
 					{aspect.pointIds.map((pointId, k) => <PointView key={pointId} {...{k, pointId, dbFuncs}} />)}
 				</section>
 			</main>
 
-			<footer>
-				<h4>{aspect.conclusion || <span className="faded">No conclusion</span>}</h4>
+			<footer className="py-3">
+				<p>{aspect.conclusion || <span className="faded">No conclusion</span>}</p>
 			</footer>
 		</section>
 	);
--- src/PointApp/Content/Home/ViewNote/ViewNote.js
+++ src/PointApp/Content/Home/ViewNote/ViewNote.js
@@ -24,7 +24,6 @@ export default function ViewNote ({note, category, goTo, dbFuncs}) {
 			<footer className="max-w-3xl mx-auto px-4 py-4 space-x-4">
 				<div>
 					<Button onClick={goTo.Dashboard}>Home</Button>
-					<Button onClick={() => goTo.Category(category.id)}>{category.title}</Button>
 					<Button onClick={() => goTo.Editor(note.id)}>Edit</Button>
 				</div>
 			</footer>
